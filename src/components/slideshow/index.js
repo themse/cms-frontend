a3372ae1353a53b31f624c7ab3e68a3a3082a196
Den <den.polus.47@gmail.com>
2020-04-26 19:04:47 +0300

    add slider without styles

--- src/components/slideshow/index.js
+++ src/components/slideshow/index.js
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  DotGroup,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 import { SlideItem } from "./item";
 import { slide } from "../../common/images";
@@ -6,21 +15,35 @@ import { slide } from "../../common/images";
 // TODO get from db
 const slides = [
   { uid: 1, name: "Apple iPad Pro", slug: "#", image: slide },
-  //   { uid: 2, name: "Apple iPhone 6S", slug: "#", image: slide },
-  //   { uid: 3, name: "Logitech K360", slug: "#", image: slide },
+  { uid: 2, name: "Apple iPhone 6S", slug: "#", image: slide },
+  { uid: 3, name: "Logitech K360", slug: "#", image: slide },
 ];
 
 export const SlideShow = () => {
   return (
-    <div id="slideshow0" className="owl-carousel" style={{ opacity: 1 }}>
-      {slides.map((slide) => (
-        <SlideItem
-          key={slide.uid}
-          name={slide.name}
-          slug={slide.slug}
-          image={slide.image}
-        />
-      ))}
-    </div>
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={30}
+      totalSlides={slides.length}
+      isPlaying
+      infinite
+      lockOnWindowScroll
+    >
+      <Slider>
+        {slides.map((slide) => (
+          <Slide key={slide.uid} index={slide.uid}>
+            <SlideItem
+              name={slide.name}
+              slug={slide.slug}
+              image={slide.image}
+            />
+          </Slide>
+        ))}
+      </Slider>
+      <DotGroup dotNumbers showAsSelectedForCurrentSlideOnly />
+
+      <ButtonBack>Back</ButtonBack>
+      <ButtonNext>Next</ButtonNext>
+    </CarouselProvider>
   );
 };
--- src/components/vendor/list.js
+++ src/components/vendor/list.js
@@ -1,17 +1,12 @@
 import React, { Fragment } from "react";
-import { vendors } from "../../common/images";
-import { VendorItem } from "./item";
+import { VendorSlider } from "./slider";
 
 // TODO get vendors from db
 export const VendorList = () => {
   return (
     <Fragment>
       <h3 className="rc-heading">We offer products from</h3>
-      {Object.entries(vendors).map(([key, value]) => (
-        <div key={key} id="carousel0" className="owl-carousel">
-          <VendorItem name={key} image={value} slug="#" />
-        </div>
-      ))}
+      <VendorSlider />
     </Fragment>
   );
 };
