f7f62d2487e7d26b815b6d6ab811c3f2917c81f2
Den <den.polusmyak@gmail.com>
2020-05-17 16:58:12 +0300

    improved redux types

--- src/store/category/actions.ts
+++ src/store/category/actions.ts
@@ -1,21 +1,10 @@
-import { Action } from '../types';
+import { typedAction } from '../types';
 import { CategoryType } from './types';
 
-export const categoryListRequest = () => ({
-    type: CategoryType.LIST_REQUEST,
-});
+export const categoryListRequest = () => typedAction<CategoryType>(CategoryType.LIST_REQUEST);
 
-export const categoryListSuccess = (categories: Array<any>) => {
-    return {
-        type: CategoryType.LIST_SUCCESS,
-        payload: categories,
-    };
-};
+export const categoryListSuccess = (categories: Array<any>) =>
+    typedAction<CategoryType, any[]>(CategoryType.LIST_SUCCESS, categories);
 
-export const categoryListFailed = (error: any) => {
-    // TODO
-    return {
-        type: CategoryType.LIST_FAILED,
-        payload: error,
-    };
-};
+export const categoryListFailed = (error: any) =>
+    typedAction<CategoryType, any>(CategoryType.LIST_FAILED, error);
--- src/store/vendor/actions.ts
+++ src/store/vendor/actions.ts
@@ -1,19 +1,10 @@
+import { typedAction } from '../types';
 import { VendorType } from './types';
 
-export const vendorListRequest = () => ({
-    type: VendorType.LIST_REQUEST,
-});
+export const vendorListRequest = () => typedAction<VendorType>(VendorType.LIST_REQUEST);
 
-export const vendorListSuccess = (vendors: Array<any>) => {
-    return {
-        type: VendorType.LIST_SUCCESS,
-        payload: vendors,
-    };
-};
+export const vendorListSuccess = (vendors: Array<any>) =>
+    typedAction<VendorType, any[]>(VendorType.LIST_SUCCESS, vendors);
 
-export const vendorListFailed = (error: any) => {
-    return {
-        type: VendorType.LIST_FAILED,
-        payload: error,
-    };
-};
+export const vendorListFailed = (error: any) =>
+    typedAction<VendorType, any>(VendorType.LIST_FAILED, error);
